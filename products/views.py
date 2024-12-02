6cfa5e58256cf8f2bd19b9c3a9cacd30b26f10f4
shubhamaragade-coditas <shubham.aragade@coditas.com>
2024-12-02 14:48:34 +0530

    Implement update API to update and retrive a  product to the inventory.

--- products/views.py
+++ products/views.py
@@ -6,3 +6,7 @@ from .serializers import ProductSerializer
 class ProductListCreateView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+class ProductRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
